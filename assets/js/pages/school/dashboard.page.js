17d4fbb5bcc7e9d9f6b33f87ada930d3b04967f9
Piggy-gry <18883748118@163.com>
2020-06-06 09:26:25 +0800

    school dashboard fix

--- assets/js/pages/school/dashboard.page.js
+++ assets/js/pages/school/dashboard.page.js
@@ -8,7 +8,6 @@ parasails.registerPage('schoolDashboard', {
     applyList:[],
     applyForm: '',
 
-<<<<<<< HEAD
     photo: '',
     imageUrl:'',
     applyID: '',
@@ -17,9 +16,6 @@ parasails.registerPage('schoolDashboard', {
     statusList: constants.APPLICATION_STATUS_INFO,
     statusChecked:constants.APPLICATION_STATUS_CHECKED,
     statusEditing:constants.APPLICATION_STATUS_EDITING,
-=======
-    statusList: constants.APPLICATION_STATUS_INFO,
->>>>>>> 24102df86342eb3c36c9a509697fae13fbca1d47
     // Server error state for the form
     cloudError: '',
   },
@@ -54,7 +50,6 @@ parasails.registerPage('schoolDashboard', {
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
     getApplyList: async function() {
-<<<<<<< HEAD
       this.applyList = await Cloud.findApplication.with();
       // console.log(this.applyList)
     },
@@ -81,64 +76,6 @@ parasails.registerPage('schoolDashboard', {
 
     getImageUrl: function(fd) {
       return '/public/avatars/' + fd; // TODO: constants
-=======
-      this.applyList = [
-        {
-          id:1,
-          name:'gry',
-          applyTime:'2020',
-          status:2
-        },
-        {
-          id:2,
-          name:'gry',
-          applyTime:'2020',
-          status:3
-        },
-        {
-          id:1,
-          name:'gry',
-          applyTime:'2020',
-          status:4
-        },
-        {
-          id:1,
-          name:'gry',
-          applyTime:'2020',
-          status:11
-        },
-      ];
-    },
-
-    applyDetail: async function(applyID) {
-      this.applyForm = {
-        name:'gry',
-        sex:'male',
-        nation:'汉族',
-        birthDate:'1997.9',
-        politicalStatus:'团员',
-        IDNumber:'11',
-        education:'11',
-        major:'11',
-        specialty:'11',
-        healthStatus:'11',
-        domicileProvince:'11',
-        pastMedicalHistory:'11',
-        domicileCity:'11',
-        domicileAddr:'11',
-        phone:'11',
-        email:'11',
-        homeAddressAndPhone:'11',
-        intention1:'330300',
-        intention2:'330322',
-        obeyTheAdjustment:true,
-        workedInTheCYL:true,
-        resume:'11',
-        volunteeringExperience:'11',
-        rewardsAndPunishment:'11',
-        school:'11'
-      };
->>>>>>> 24102df86342eb3c36c9a509697fae13fbca1d47
     },
 
   }
