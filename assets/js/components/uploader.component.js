912767fed90ffd202e5a35d9ee7d82f49e09148c
hduleil <tcxll126@126.com>
2020-06-06 15:34:57 +0800

    image zoom [148, 196]

--- assets/js/components/uploader.component.js
+++ assets/js/components/uploader.component.js
@@ -72,10 +72,16 @@ parasails.registerComponent('uploader', {
         //   $('#imageResult').attr('src', e.target.result);
         // };
         // reader.readAsDataURL(event.target.files[0]);
-        console.log(event.target.files[0]);
+        const settings = {
+          file: event.target.files[0],
+          maxWidth: 148,
+          maxHeight: 196
+        };
+        const resizedimage = await this.resizeImage(settings);
+
         let fd;
         try {
-          fd = await Cloud.uploadPhoto.with({id: 1, photo:event.target.files[0]});
+          fd = await Cloud.uploadPhoto.with({id: 1, photo: resizedimage});
         } catch (e) {
           console.log(e);
           // TODO: show toast
@@ -84,48 +90,71 @@ parasails.registerComponent('uploader', {
         if (fd) {
           this.$emit('update:photo', fd);
         }
-
       }
     },
 
-    // resize: async function (file) {
-    //   // Ensure it's an image
-    //   // if(file.type.match(/image.*/)) {
-    //   //   console.log('An image has been loaded');
+    resizeImage: async function (settings) {
+      var file = settings.file;
+      var maxWidth = settings.maxWidth;
+      var maxHeight = settings.maxHeight;
+      var reader = new FileReader();
+      var image = new Image();
+      var canvas = document.createElement('canvas');
 
-    //   //   // Load the image
-    //   //   var reader = new FileReader();
-    //   //   reader.onload = function (readerEvent) {
-    //   //     var image = new Image();
-    //   //     image.onload = function (imageEvent) {
+      // var dataURItoBlob = function (dataURI) {
+      //   var bytes = dataURI.split(',')[0].indexOf('base64') >= 0 ?
+      //         atob(dataURI.split(',')[1]) :
+      //         unescape(dataURI.split(',')[1]);
+      //   var mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+      //   var max = bytes.length;
+      //   var ia = new Uint8Array(max);
+      //   for (var i = 0; i < max; i++)
+      //   {ia[i] = bytes.charCodeAt(i);}
+      //   return new Blob([ia], { type: mime });
+      // };
 
-    //   //       // Resize the image
-    //   //       var canvas = document.createElement('canvas');
-    //   //       var max_width = 1200;
-    //   //       var max_height = 1200;
-    //   //       var width = image.width;
-    //   //       var height = image.height;
-    //   //       if (width > height) {
-    //   //         if (width > max_size) {
-    //   //           height *= max_size / width;
-    //   //           width = max_size;
-    //   //         }
-    //   //       } else {
-    //   //         if (height > max_size) {
-    //   //           width *= max_size / height;
-    //   //           height = max_size;
-    //   //         }
-    //   //       }
-    //   //       canvas.width = width;
-    //   //       canvas.height = height;
-    //   //       canvas.getContext('2d').drawImage(image, 0, 0, width, height);
-    //   //       resizedImage = canvas.toDataURL('image/jpeg');
-    //   //     };
-    //   //     image.src = readerEvent.target.result;
-    //   //   };
-    //   //   reader.readAsDataURL(file);
-    //   // }
-    // }
+      var dataURItoFile = function (dataURI) {
+        var bytes = dataURI.split(',')[0].indexOf('base64') >= 0 ?
+              atob(dataURI.split(',')[1]) :
+              unescape(dataURI.split(',')[1]);
+        var mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        var max = bytes.length;
+        var ia = new Uint8Array(max);
+        for (var i = 0; i < max; i++)
+        {ia[i] = bytes.charCodeAt(i);}
+        return new File([ia], file.name, { type: mime });
+      };
 
+      var resize = function () {
+        var width = image.width;
+        var height = image.height;
+        var aspectRatio = width / height;
+        if (aspectRatio > maxWidth / maxHeight) {
+          width = maxWidth;
+          height = width / aspectRatio;
+        } else {
+          height = maxHeight;
+          width = height * aspectRatio;
+        }
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+        var dataUrl = canvas.toDataURL('image/jpeg');
+        // return dataURItoBlob(dataUrl);
+        return dataURItoFile(dataUrl);
+      };
+
+      return new Promise(((ok, no) => {
+        if (!file.type.match(/image.*/)) {
+          no(new Error('Not an image'));
+          return;
+        }
+        reader.onload = function (readerEvent) {
+          image.onload = function () { return ok(resize()); };
+          image.src = readerEvent.target.result;
+        };
+        reader.readAsDataURL(file);
+      }));
+    }
   }
 });
