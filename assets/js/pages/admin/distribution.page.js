c223843b4b80de64c4f2772bf836178ce4dcb8b6
Piggy-gry <18883748118@163.com>
2020-06-16 22:26:21 +0800

    vaptcha

--- assets/js/pages/admin/distribution.page.js
+++ assets/js/pages/admin/distribution.page.js
@@ -1,12 +1,30 @@
 parasails.registerPage('distribution', {
   data: {
-    stats:[],
+    posts:[],
   },
 
   beforeMount: async function() {
-
+    this.getPosts();
   },
 
   methods: {
+    getPosts: async function() {
+      // this.posts = await Cloud.findPosts.with();
+      this.posts = [
+        {
+          id: 1,
+          name:'后勤'
+        },
+        {
+          id: 2,
+          name:'支教'
+        },
+        {
+          id: 3,
+          name:'助理'
+        }
+      ];
+    },
+
   }
 });
--- assets/js/pages/entrance/login.page.js
+++ assets/js/pages/entrance/login.page.js
@@ -42,6 +42,7 @@ parasails.registerPage('login', {
   },
   mounted: async function() {
     //…
+    await this.vaptchaSet();
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -57,6 +58,19 @@ parasails.registerPage('login', {
       localStorage.removeItem('applyForm');
       window.location = '/';
     },
+
+    vaptchaSet: async function() {
+      console.log('qqq');
+      window.vaptcha({
+        //配置参数
+        vid: '5ee73be818501124667130e3', // 验证单元id
+        type: 'click', // 展现类型 点击式
+        container: '#vaptchaContainer'// 按钮容器，可为Element 或者 selector
+      }).then((vaptchaObj) => {
+        console.log('qqq');
+        vaptchaObj.render();// 调用验证实例 vaptchaObj 的 render 方法加载验证按钮
+      });
+    }
 
   }
 });
