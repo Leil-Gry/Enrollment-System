006611b3e175ffd7674398240259728065ca8e89
hduleil <tcxll126@126.com>
2020-06-23 16:00:00 +0800

    岗位修改、删除完成

--- api/controllers/admin/update-position.js
+++ api/controllers/admin/update-position.js
@@ -7,16 +7,37 @@ module.exports = {
   description: 'update Position.',
 
   inputs: {
+    newName: {
+      type: 'string',
+      required: true
+    },
+    id:{
+      type:'number',
+      required: true
+    }
 
   },
 
   exits: {
+    success: {
+      responseType: 'ok'
+    },
 
-
+    updateErr: {
+      responseType: 'string'
+    }
   },
 
-  fn: async function () {
+  fn: async function (inputs) {
+    var update = await Position.updateOne({ id: inputs.id })
+      .set({
+        name: inputs.newName
+      });
 
+    if (!update) {
+      throw 'updateErr';
+    }
+
+    return 'success';
   }
-
 };
--- assets/js/pages/admin/distribution.page.js
+++ assets/js/pages/admin/distribution.page.js
@@ -3,6 +3,7 @@ parasails.registerPage('distribution', {
     posts:[],
     activeUpdateForm: false,
     activeDeleteConfirm: false,
+    oldPositionName:'',
     newPositionName: '',
     currentPositionId: '',
     deletePositionId: ''
@@ -19,21 +20,28 @@ parasails.registerPage('distribution', {
     },
 
     deletePosition: async function() {
-      // TODO: 完善API细节
       await Cloud.deletePosition.with({ id: this.deletePositionId });
       this.deletePositionId = '';
       this.activeDeleteConfirm = false;
+      window.history.go(0);
     },
 
     updatePosition: async function() {
+      if (this.oldPositionName === this.newPositionName) {
+        // ShowTip('当前输入的岗位名称相同！','danger');
+        return;
+      }
+
       await Cloud.updatePosition.with({ id: this.currentPositionId, newName: this.newPositionName }); // TODO: 完成这个API，完善边缘逻辑，如错误提示等
       this.activeUpdateForm = false; // 关闭弹窗
+      ShowTip('修改成功！','success');
+      window.history.go(0);
     },
 
     showUpdateForm: async function(position) {
-      // TODO: position中有 id name application  这里做判断，如果新修改名和旧一致则不修改 ，弹窗报告  等， js里用 === 代表等于 !== 代表不等于
-      this.currentPositionId = position.Id;
+      this.currentPositionId = position.id;
       this.newPositionName = position.name;
+      this.oldPositionName = position.name;
       this.activeUpdateForm = true;
     },
 
